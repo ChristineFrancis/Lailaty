534f80c7702375bcdb4460ba4e9c0838462c0c8b
danitoo <edk6967@gmail.com>
2024-11-11 14:22:13 +0300

    string manager

---
+++ lib/core/resources/string_manager.dart
@@ -0,0 +1,17 @@
+class StringManager {
+  static String carInfo = "معلومات السيارة";
+  static String typeOfCar = "نوع السيارة : ";
+  static String editingCar = "إذا كانت سيارتك معدلة";
+  static String photoCar = "اضف صور السيارة :";
+  static String addAroundphotoCar = "صور جانبية عدد 2:";
+  static String addBackphotoCar = "صورة خلفية:";
+  static String addFrontphotoCar = "صورة أمامية";
+  static String addInsidephotoCar = "صورة داخلية:";
+  static String photoLecienceCar = "اضف صور رخصة السيارة :";
+  static String photoFrontBackLecienceCar = "صورة خلفية وصورة أمامية :";
+  static String tam = "تم";
+  static String pressHere = "اضغط هنا";
+  static String questionToContactWithClientService = "إذا كانت لديك أسئلة يرجى التواصل مع ";
+  static String clientService = "خدمة العملاء";
+
+}
---
+++ lib/feature/agreeScreens/presentation/view/carInfo.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CarInfoView extends StatelessWidget {
+  const CarInfoView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(),body: Directionality(
+      textDirection: TextDirection.rtl,
+      child: Column(
+        children: [
+          Row(children: [
+    
+          ],)
+        ],
+      ),
+    ),);
+  }
+}
