0e8300407b28ea1f1e254b5a22cec5894605cf51
Salamah_Salamah <salamah.s.salamah03@gmail.com>
2024-11-12 20:17:30 +0200

    implemented remote_data_source.dart

--- lib/common features/authentication/data/data_sources/remote_data_source.dart
+++ lib/common features/authentication/data/data_sources/remote_data_source.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
+
 import 'package:dartz/dartz.dart';
+import 'package:lailaty/common%20features/authentication/data/models/user_model.dart';
 import 'package:lailaty/common%20features/authentication/data/models/verification_model.dart';
 import 'package:http/http.dart' as http;
-import 'package:lailaty/common%20features/authentication/domain/entities/user.dart';
+import 'package:lailaty/core/error_manager/exception.dart';
 
 abstract class RemoteDataSource {
-  Future<Unit> addUser(User user);
+  Future<Unit> addUser(UserModel userModel);
   Future<List<VerificationModel>> getVerificationCode();
 }
 
@@ -14,16 +17,40 @@ class RemoteDataSourceImpl implements RemoteDataSource {
   final http.Client client;
 
   RemoteDataSourceImpl({required this.client});
-  
+
   @override
-  Future<Unit> addUser(User user) {
-    // TODO: implement addUser
-    throw UnimplementedError();
+  Future<Unit> addUser(UserModel userModel) async {
+    final body = {
+      "phoneNumber": userModel.phoneNumber,
+      "password": userModel.password,
+      "firstName": userModel.firstName,
+      "lastName": userModel.lastName,
+    };
+
+    final response = await client.post(Uri.parse(BASE_URL + "/users/"),
+        body: body); // TODO: manage the extension
+    if (response.statusCode == 201) {
+      return Future.value(unit);
+    } else {
+      throw ServerException();
+    }
   }
-  
+
   @override
-  Future<List<VerificationModel>> getVerificationCode() {
-    // TODO: implement getVerificationCode
-    throw UnimplementedError();
+  Future<List<VerificationModel>> getVerificationCode() async {
+    final response = await client.get(
+        Uri.parse(
+            BASE_URL + "/verification_code/"), // TODO: manage the extension
+        headers: {"Content-Type": "application/json"});
+
+    if (response.statusCode == 200) {
+      final List decodedJson = json.decode(response.body) as List;
+      final List<VerificationModel> verificationModels = decodedJson
+          .map<VerificationModel>((jsonVerificationModel) => VerificationModel.fromJson(jsonVerificationModel))
+          .toList();
+      return verificationModels;
+    } else {
+      throw ServerException();
+    }
   }
 }
