f02fcf99ba3a4457ac08e1230e05b48e89cc5bf8
Salamah_Salamah <salamah.s.salamah03@gmail.com>
2024-11-22 17:26:51 +0200

    put Segeo as defult font

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: "Segeo",
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: false,
       ),
