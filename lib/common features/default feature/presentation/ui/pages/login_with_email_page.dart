b7c51e75ce6236ea34c2df67741ab25e6c9979cf
Salamah_Salamah <salamah.s.salamah03@gmail.com>
2025-02-26 23:08:48 +0200

    a

---
+++ lib/common features/default feature/presentation/ui/pages/login_with_email_page.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:lailaty/common%20features/authentication/presentation/ui/pages/verification_code_page.dart';
+import 'package:lailaty/common%20features/authentication/presentation/ui/widgets/lailaty_arabic_and_english.dart';
+import 'package:lailaty/common%20features/authentication/presentation/ui/widgets/login_word.dart';
+import 'package:lailaty/core/resources/asset_manager.dart';
+import 'package:lailaty/core/resources/color_manager.dart';
+
+import '../widgets/custom widgets/text widgets/custom_text_widget.dart';
+import '../widgets/custom_button.dart';
+
+class LoginWithEmailPage extends StatelessWidget {
+  const LoginWithEmailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: ColorManager.grey,
+          leading: IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              icon: Icon(
+                Icons.arrow_back,
+                color: ColorManager.yellow,
+              )),
+        ),
+        backgroundColor: ColorManager.grey,
+        body: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.sizeOf(context).width / 45,
+          ),
+          child: Column(
+            children: [
+              Spacer(
+                flex: 1,
+              ),
+              LoginWord(),
+              Spacer(
+                flex: 3,
+              ),
+              LayoutBuilder(
+                builder: (context, constraints) {
+                  final imageHeight = constraints.maxWidth / 2.3;
+                  return Directionality(
+                    textDirection: TextDirection.ltr,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        LailatyArabicAndEnglish(),
+                        SizedBox(
+                          width: 6,
+                        ),
+                        Container(
+                          width: 3, // Width of the yellow line
+                          height: imageHeight, // Match the height of the image
+                          color: ColorManager.yellow,
+                        ),
+                        SizedBox(
+                          width: 6,
+                        ),
+                        SvgPicture.asset(
+                          ImageAssetManager.loginAmico,
+                          width: imageHeight, // Set the width of the image
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+              Spacer(
+                flex: 3,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.sizeOf(context).width / 20),
+                child: CustomTextWidget(
+                    text:
+                        'سجل دخولك باستخدام Google لتسجيل بريدك الالكتروني و اسمك',
+                    color: Colors.black,
+                    fontSize: 26),
+              ),
+              //SizedBox(height: 20,),
+              //Spacer(flex: 1,),
+
+              //CityDropdown(),
+
+              Spacer(
+                flex: 3,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.sizeOf(context).width / 9,
+                ),
+                child: CustomButton(
+                  textButton: 'التسجيل عن طريق Google',
+                  textSize: 19,
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => VerificationCodePage()));
+                  },
+                ),
+              ),
+
+              Spacer(
+                flex: 3,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/common features/default feature/presentation/ui/widgets/custom_button.dart
+++ lib/common features/default feature/presentation/ui/widgets/custom_button.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lailaty/common%20features/default%20feature/presentation/ui/widgets/custom%20widgets/text%20widgets/custom_text_widget.dart';
 import 'package:lailaty/core/resources/color_manager.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CustomButton extends StatelessWidget {
   final String textButton;
