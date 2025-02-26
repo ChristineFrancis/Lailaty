f97a16be0af4b9ff9601263baa6cbe2434053deb
Salamah_Salamah <salamah.s.salamah03@gmail.com>
2025-02-26 22:40:52 +0200

    a

---
+++ lib/core/resources/color_manager.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class ColorManager {
+  static const Color grey = Color(0xffB2B1AD);
+  static const Color yellow = Color(0xffFFFC01);
+  static const Color greyForSwitch = Color(0xffC7C7C7);
+  static const Color darkGrey = Color(0xff707070);
+  static const Color grey200 = Color(0xffBCBBB8);
+  static const Color grey100 = Color(0xfff5f5f5);
+  static const Color blue = Color(0xff0700BE);
+}
